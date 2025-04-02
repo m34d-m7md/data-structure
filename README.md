a314f3e35ce4558825f1990394621a334f1a27b9
Mohad Elmasaudi <mohadelmasaudi@gmail.com>
2025-04-02 16:55:09 +0200

    add cmake

---
+++ README.md
@@ -0,0 +1,3 @@
+# this is data structure library and can used for testing
+
+# start edit src/main.cpp for testing and run `cmake --build bin` then ./bin/app
--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 using data_structure::Sort;
 
 int main() {
-  double arr[] = {3.4, 1.4, 6.3, 2.4, 1.3, 5.6, 2.9, 0.5, 3.6};
+  double arr[] = {3.4, 1.4, 6.3, 2.4, 1.3, 5.6, 2.9, 0.5, 3.6, 6.4, 3.6, 2.7, 5.3};
 
-  Sort<double>::bubble(arr, 9);
+  Sort<double>::bubble(arr, 13);
 
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < 13; i++) {
     printf("# %.1lf ", arr[i]);
   }
 
